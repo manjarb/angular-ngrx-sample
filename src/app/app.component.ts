a76466b6553fe57f1608862e0d022915c46d5417
Varis Darasirikul <varis.dara@gmail.com>
2019-12-08 22:08:50 +0800

    Add simple layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+interface ITodo {
+  label: string;
+  complete: boolean;
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'angular-ngrx-sample';
+  title = "angular-ngrx-sample";
+  collection: ITodo[] = [];
+
+  addTodo(input) {
+    if (!input.value.trim()) return;
+    const payload = { label: input.value, complete: false };
+
+    console.log(payload);
+
+    input.value = "";
+  }
+
+  deleteTodo() {}
 }

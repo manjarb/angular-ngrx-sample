d09a33b356101b7ddaaf788e953220e1d8269bf6
Varis Darasirikul <varis.dara@gmail.com>
2019-12-08 22:48:12 +0800

    Init app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { StoreModule } from "@ngrx/store";
 
-import * as fromStore from "./store";
+import { StoreModule, MetaReducer } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
 
+import { environment } from "../environments/environment";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
-const store = new fromStore.Store(
-  {},
-  {
-    todos: [{ label: "Eat pizza", complete: false }]
-  }
-);
+// not used in production
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { storeFreeze } from "ngrx-store-freeze";
+
+export const metaReducers: MetaReducer<any>[] = !environment.production
+  ? [storeFreeze]
+  : [];
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot(store.action)],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, { metaReducers }),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })

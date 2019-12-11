69324b7d2cab8544eaaf24867201b13b12f9fc6d
Varis Darasirikul <varis@easyship.com>
2019-12-11 16:48:55 +0800

    Combine multiple effect

--- src/app/store/actions/index.ts
+++ src/app/store/actions/index.ts
@@ -1,2 +1,3 @@
 export * from "./pizzas.action";
 export * from "./topping.action";
+export * from "./router.action";
--- src/app/store/effects/pizzas.effect.ts
+++ src/app/store/effects/pizzas.effect.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@angular/core";
 import { Actions, ofType, Effect } from "@ngrx/effects";
 
+import * as fromRoot from "../../store";
 import * as pizzaActions from "../actions/pizzas.action";
 import * as fromServices from "../../../products/services";
 
 import { switchMap, map, catchError } from "rxjs/operators";
-import { of } from "rxjs";
+import { of, from } from "rxjs";
 
 @Injectable()
 export class PizzasEffects {
@@ -38,6 +39,18 @@ export class PizzasEffects {
   );
 
   @Effect()
+  createPizzaSuccess$ = this.actions$.pipe(
+    ofType(pizzaActions.CREATE_PIZZA_SUCCESS),
+    map((action: pizzaActions.CreatePizzaSuccess) => action.payload),
+    map(
+      pizza =>
+        new fromRoot.Go({
+          path: ["/products", pizza.id]
+        })
+    )
+  );
+
+  @Effect()
   updatePizza$ = this.actions$.pipe(
     ofType(pizzaActions.UPDATE_PIZZA),
     map((action: pizzaActions.UpdatePizza) => action.payload),
@@ -58,6 +71,19 @@ export class PizzasEffects {
         map(() => new pizzaActions.RemovePizzaSuccess(pizza)),
         catchError(error => of(new pizzaActions.RemovePizzaFail(error)))
       );
+    })
+  );
+
+  @Effect()
+  handlePizzaSuccess$ = this.actions$.pipe(
+    ofType(
+      pizzaActions.UPDATE_PIZZA_SUCCESS,
+      pizzaActions.REMOVE_PIZZA_SUCCESS
+    ),
+    map(pizza => {
+      return new fromRoot.Go({
+        path: ["/products"]
+      });
     })
   );
 }
